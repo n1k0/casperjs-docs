fc4c4db205e0dde70cca0f5213b59cf62e2b7037
Nicolas Perriault <nperriault@gmail.com>
2013-01-03 09:13:53 +0100

    pygments style through theme configuration

--- conf.py
+++ conf.py
@@ -77,7 +77,7 @@ exclude_patterns = ['_build', 'README.md']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'vim'
+#pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
